3fb893ff97acc9d8bbe2ef54012773bb63bb1872
Lokesh <lokesh22fhg@gmail.com>
2025-08-12 15:58:28 +0530

    7

--- week-7(recursion)/Practice questions/lastocc.cpp
+++ week-7(recursion)/Practice questions/lastocc.cpp
@@ -1,2 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
+int lastocc(string &str,char ch,int i){
+    if(i<0) return -1;
+    if(str[i]==ch){
+        return i;
+    }
+    if(i<0) return -1;
+    return lastocc(str,ch,i-1);
+}
+void lastocc(string &str,char ch,int i,int &ans){
+    if(i>=str.size()) return;
+    if(str[i]==ch){
+        ans=i;
+    }
+    lastocc(str,ch,i+1,ans);
+}
+
+int main {
+    string str="abcd";
+    char ch='d';
+    //int i=0;
+    //int ans=-1;
+    cout<<lastocc(str,ch,str.length()-1);
+    return 0;
+}
