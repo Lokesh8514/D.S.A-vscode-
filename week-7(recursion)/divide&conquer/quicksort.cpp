475424975fef18cc68c6439a5b7502a0dddab3eb
Lokesh <lokesh22fhg@gmail.com>
2025-09-09 21:17:43 +0530

    rsf

--- week-7(recursion)/divide&conquer/quicksort.cpp
+++ week-7(recursion)/divide&conquer/quicksort.cpp
@@ -1,76 +1,44 @@
-#include <iostream>
-#include <vector>
+#include<bits/stdc++.h>
 using namespace std;
 
-int partitionhighaspivot(vector<int>& arr, int low, int high) {
+// int partitionhighaspivot(vector<int>& arr, int low, int high) {
+//     int pivot = arr[high];
+//     int i = low - 1;
+//     for (int j = low; j <= high - 1; j++) {
+//         if (arr[j] < pivot) {
+//             i++;
+//             swap(arr[i], arr[j]);
+//         }
+//     }
+//     swap(arr[i + 1], arr[high]);  
+//     return i + 1;
+// }
 
-    // choose the pivot
-    int pivot = arr[high];
-  
-    // undex of smaller element and indicates 
-    // the right position of pivot found so far
-    int i = low - 1;
-
-    // Traverse arr[low..high] and move all smaller
-    // elements on left side. Elements from low to 
-    // i are smaller after every iteration
-    for (int j = low; j <= high - 1; j++) {
-        if (arr[j] < pivot) {
-            i++;
-            swap(arr[i], arr[j]);
-        }
-    }
-    
-    // move pivot after smaller elements and
-    // return its position
-    swap(arr[i + 1], arr[high]);  
-    return i + 1;
-}
 int partitionlowaspivot(vector<int>& arr, int low, int high) {
-
-    // choose the pivot
     int pivot = arr[low];
-  
-    // undex of smaller element and indicates 
-    // the right position of pivot found so far
     int k = high;
-
-    // Traverse arr[low..high] and move all smaller
-    // elements on left side. Elements from low to 
-    // i are smaller after every iteration
-    for (int j = high; j >low; j++) {
-        if (arr[j] < pivot) {
-            swap(arr[k], arr[j]);
+    for (int j = high; j > low; j--) {
+        if (arr[j] > pivot) {
+            swap(arr[j], arr[k]);
             k--;
         }
     }
-    
-    // move pivot after smaller elements and
-    // return its position
     swap(arr[k], arr[low]);  
     return k;
 }
 
-// the QuickSort function implementation
 void quickSort(vector<int>& arr, int low, int high) {
-  
     if (low < high) {
-      
-        // pi is the partition return index of pivot
         int pi = partitionlowaspivot(arr, low, high);
-        //int pi = partitionhighaspivot(arr, low, high);
-        // recursion calls for smaller elements
-        // and greater or equals elements
         quickSort(arr, low, pi - 1);
         quickSort(arr, pi + 1, high);
     }
 }
 
 int main() {
-    vector<int> arr = {10, 7, 8, 9, 1, 5};
+    vector<int> arr = {8,3,4,1,20,50,30};
     int n = arr.size();
     quickSort(arr, 0, n - 1);
-  
     for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
